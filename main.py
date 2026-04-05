5e7464cd60be0e99a8a26ab028a72df61fb78823
matan-abir <matan.abir@gmail.com>
2026-04-06 00:44:39 +0300

    Integrated sysinfo and made aesthetic fixes to main.py

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
+import sysinfo
+
 print("===== Devops Dashboard =====")
+
+menu_message = """
+– [1] System Info
+– [2] Log Checker
+– [3] Task List
+– [0] Exit
+ Input your choice between 0 and 3 now: 
+"""
 
 menu_choice = -1
 while(menu_choice != 0):
 	try:
-		print("""
-		– [1] System Info
-		– [2] Log Checker
-		– [3] Task List
-		– [0] Exit
-		 Input your choice between 0 and 3 now: 
-		""")
+		print(menu_message)
 		menu_choice = int(input())
 		if menu_choice >= 0 and menu_choice <=3:
 			# Valid input
-			if menu_choice != 0:
-				input("Coming soon... Press enter to return... ")
+			if menu_choice == 1:
+				sysinfo.show_sysinfo()
+			else:
+				print("Coming soon...!")
 		else:
 			# Invalud input
 			input("Invalid choice! Press enter to try again..")
--- sysinfo.py
+++ sysinfo.py
@@ -4,4 +4,4 @@ import socket
 def show_sysinfo():
   print(f"Current Working Directory: {os.getcwd()}")
   print(f"Hostname: {socket.gethostname()}")
-  print(f"Username: {os.getlogin()})
+  print(f"Username: {os.getlogin()}")
