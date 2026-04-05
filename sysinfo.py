fee6edd282996407400ff336e4300a93724de087
Matan-Abir <132847972+Matan-Abir@users.noreply.github.com>
2026-04-06 00:09:28 +0300

    Add sysinfo.py to display system information

---
+++ sysinfo.py
@@ -0,0 +1,7 @@
+import os
+import socket
+
+def show_sysinfo():
+  print(f"Current Working Directory: {os.getcwd()}")
+  print(f"Hostname: {socket.gethostname()}")
+  print(f"Username: {os.getlogin()})
