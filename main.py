15c5eb9e72d72677e3dda729bba9f15623a90ec1
matan-abir <matan.abir@gmail.com>
2026-04-05 23:44:26 +0300

    Added main.py, mostly a 'features to be added' version

---
+++ main.py
@@ -0,0 +1,24 @@
+print("===== Devops Dashboard =====")
+
+menu_choice = -1
+while(menu_choice != 0):
+	try:
+		print("""
+		– [1] System Info
+		– [2] Log Checker
+		– [3] Task List
+		– [0] Exit
+		 Input your choice between 0 and 3 now: 
+		""")
+		menu_choice = int(input())
+		if menu_choice >= 0 and menu_choice <=3:
+			# Valid input
+			if menu_choice != 0:
+				input("Coming soon... Press enter to return... ")
+		else:
+			# Invalud input
+			input("Invalid choice! Press enter to try again..")
+	except:
+		input("Input has to be a number between 0 and 3! Press enter to try again..")
+
+input("Now exiting the devops dashboard...")
